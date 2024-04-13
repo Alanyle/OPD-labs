0c78af33fc47565ed92070801060da06cad1ca64
Alan <alanyle@mail.ru>
2024-04-13 23:57:39 +0300

    Finance Bot Done in main
    He does loan, deposit calc and 52

--- L2V11.py
+++ L2V11.py
@@ -1,11 +1,74 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '7148468736:AAGjwGoJAfKZdBO_SpqX5koycYldnZL7qdc'
+BOT_USERNAME: Final =  '@Al_FinnBot'
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hey there! I am a finance bot. Here are some of the commands: \n"
+                                    "credc - for credit calculator\n"
+                                    "depc - for deposit calculator\n"
+                                    "game - for 52\n")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Just press start(or write '/start')")
+
+def handle_response(text: str) -> str:
+    prc: str = text.lower()
+    if 'credc' in prc:
+        return 'write your credit amount'
+    if 'depc' in prc:
+        return 'deposit ur moni pls'
+    if 'game' in prc:
+        return '52 52 52 52'
+
+    return 'what?'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print (f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print (f'update {update} caused error {context.error}')
+
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    app.add_error_handler(error)
+
+    print('Polling...')
+    app.run_polling(poll_interval=5)
+
+'''
 import logging
 import time
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
 import Parser
 
 
 def Bot():
-    bot = Bot(token="Ваш токен")  # Объект бота
+    bot = Bot(token="7148468736:AAGjwGoJAfKZdBO_SpqX5koycYldnZL7qdc")  # Объект бота
     dp = Dispatcher(bot)  # Диспетчер
 
 @dp.message_handler(commands=['start', 'help'])
@@ -15,3 +78,4 @@ async def process_start_command(message: types.Message):
 @dp.message_handler(commands=['command'])
 async def process_start_command(message: types.Message):
     await message.reply("Тут еще что-то")
+'''
--- Parser.py
+++ Parser.py
@@ -12,15 +12,19 @@ def parse():
         soup = BS(page.text, "html.parser") # передаем страницу в bs4
         block = soup.findAll('div', class_="news-card__content") # находим  контейнер с нужным классом
         description = ''
+        date = ''
         if (len(block)):
+            print('page = ' + str(g))
             for data in block: # проходим циклом по содержимому контейнера
                 if data.find('h3'): # находим тег <p>
                     description = data.text # записываем в переменную содержание тега
                 if description!='':
-                    print(description)
+                    text = description[:11] + description[45:-36]
+                    print(text)
                     with open("test.txt", "a", encoding="utf-8") as file:
-                        file.write(description)
+                        file.write(text)
             g+=1
         else:
             break
-parse()
+if __name__ == '__main__':
+    parse()
